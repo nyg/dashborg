cf814126ecf4725cddbc389b37004cf8ff1fb4ed
nyg <nyg@users.noreply.github.com>
2021-05-23 19:43:59 +0200

    Refactor Smart Yield earnings page

--- pages/api/yield-earning.js
+++ pages/api/yield-earning.js
@@ -1,41 +1,113 @@
 import { PrismaClient } from '@prisma/client'
 
-
+// TODO
+// - put yield transfer in ledger?
+// - keep use of Decimal.js
 export default async (req, res) => {
 
   const prisma = new PrismaClient()
   await prisma.$connect()
 
-  const earnings = await prisma.ledgerEntry.findMany({
-    where: {
-      type: { value: 'Yield' }
-    },
-    select: {
-      time: true,
-      ccy: { select: { code: true } },
-      amount: true
-    }
-  })
+  /* Retrieve yield transfers and earnings from database. */
 
-  res.status(200).json({ status: 'Success', earnings: earnings })
+  const transfers = await prisma
+    .yieldTransfer.findMany({
+      select: {
+        completionDate: true,
+        account: true,
+        asset: { select: { code: true } },
+        amount: true
+      },
+      orderBy: { completionDate: 'asc' }
+    })
+
+  const earnings = await prisma
+    .ledgerEntry.findMany({
+      select: {
+        time: true,
+        asset: { select: { code: true } },
+        amount: true
+      },
+      where: { type: { value: 'Yield' } },
+      orderBy: { time: 'asc' }
+    })
+
+  await prisma.$disconnect()
+
+  /* Arrange transfer dates by asset and account. */
+
+  const transfersByAsset = transfers
+    .reduce((transfers, transfer) => {
+
+      // init keys
+      transfers[transfer.asset.code] ??= {}
+      transfers[transfer.asset.code][transfer.account] ??= { startDates: [] }
+
+      transfers[transfer.asset.code][transfer.account].startDates.push(transfer.completionDate.getTime())
+      return transfers
+    }, {})
 
 
-  // const ledger = sb.getLedgerEntries(wb)
-  // const allEarnings = sb.getEarnings(ledger)
-  // const assets = [...new Set(allEarnings.map(f => f.currency))]
+  /* Arrange transfers by date, asset and account. */
 
-  // const earnings = assets.map(asset => {
+  const transfersByDate = transfers
+    .reduce((transfers, transfer) => {
 
-  //   const assetEarnings = allEarnings.filter(e => e.currency == asset)
-  //   const dayCount = assetEarnings.length
-  //   const totalEarned = assetEarnings.reduce((acc, e) => acc + e.netAmount, 0)
+      const date = transfer.completionDate.getTime()
+      const asset = transfer.asset.code
 
-  //   return {
-  //     asset: asset,
-  //     dayCount: dayCount,
-  //     totalEarned: totalEarned.toFixed(2),
-  //     dailyAverage: (totalEarned / dayCount).toFixed(2),
-  //     yearlyEstimate: (totalEarned / dayCount * 365).toFixed(2)
-  //   }
-  // })
+      // init keys
+      transfers[date] ??= {}
+      transfers[date][asset] ??= {}
+      transfers[date][asset][transfer.account] ??= {}
+
+      transfers[date][asset][transfer.account] = {
+        balance: 0,
+        transfered: transfer.amount.toNumber()
+      }
+
+      return transfers
+    }, {})
+
+  /* Arrange earnings by date, asset and account. */
+
+  const earningsByDate = earnings
+    .reduce((earnings, earning) => {
+
+      const asset = earning.asset.code
+      const today = earning.time.getTime()
+      const yesterday = new Date(today - 86_400_000).getTime()
+
+      // compute the balance of all accounts for the previous day and current asset
+      const accounts = Object.keys(earnings[yesterday][asset])
+      const totalBalance = accounts.reduce((totalBalance, account) =>
+        totalBalance + earnings[yesterday][asset][account].balance + earnings[yesterday][asset][account].transfered, 0)
+
+      // init keys
+      earnings[today] ??= {}
+      earnings[today][asset] ??= {}
+
+      accounts.forEach(account => {
+
+        // init account balance
+        earnings[today][asset][account] ??= {
+          balance: 0,
+          transfered: 0
+        }
+
+        // today's balance = yesterday's balance
+        //                 + yesterday's transfered amount if any
+        //                 + daily yield earning pro rata of yesterday's account balance
+        const yesterdayBalance = earnings[yesterday][asset][account].balance + earnings[yesterday][asset][account].transfered
+        earnings[today][asset][account].balance = yesterdayBalance
+          + (yesterdayBalance / totalBalance) * earning.amount.toNumber()
+
+        // update last date for which yield was received for the account
+        transfersByAsset[asset][account].endDate = today
+      })
+
+      return earnings
+    }, transfersByDate)
+
+  res.status(200).json({ status: 'Success', periods: transfersByAsset, earnings: earningsByDate })
 }
--- pages/yield.js
+++ pages/yield.js
@@ -1,6 +1,8 @@
+import { Fragment } from 'react'
+import useSWR from 'swr'
 import Head from 'next/head'
 import Layout from '../components/layout'
-import Amount from '../components/amount'
+import * as format from '../utils/format'
 
 export default function Yield() {
 
@@ -15,29 +17,83 @@ export default function Yield() {
     earnings = <div className="text-center pt-4">Loading data…</div>
   }
   else {
+    console.dir(Object.keys(data.periods))
     earnings = (
-      <table className="w-full">
-        <thead>
-          <tr className="text-right">
-            <th className="text-left">Asset</th>
-            <th>Number of Days</th>
-            <th>Total Earned</th>
-            <th>Daily Average</th>
-            <th>Yearly Estimate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.earnings.map(earning => (
-            <tr key={earning.asset} className="text-right">
-              <td className="text-left">{earning.asset}</td>
-              <td>{earning.dayCount}</td>
-              <td><Amount value={earning.totalEarned} /></td>
-              <td><Amount value={earning.dailyAverage} /></td>
-              <td><Amount value={earning.yearlyEstimate} /></td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <Fragment>
+        {Object.keys(data.periods).map(asset => (
+          <Fragment key={asset}>
+            {Object.keys(data.periods[asset]).map(account => {
+              let totalDayCount = 0
+              let totalSubscription = 0
+              let totalYieldEarned = 0
+              let firstBalance
+              let lastBalance
+              return (
+                <table key={`${asset}-${account}`} className="w-full">
+                  <caption>{asset} — {account}</caption>
+                  <thead>
+                    <tr className="text-right">
+                      <th className="text-left">From</th>
+                      <th className="text-left">To</th>
+                      <th>Days</th>
+                      <th>Subscription</th>
+                      <th>Start balance</th>
+                      <th>End balance</th>
+                      <th>Yield earned</th>
+                      <th>Daily average</th>
+                      <th>Approx rate</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {data.periods[asset][account].startDates.map((startDate, index, startDates) => {
+
+                      const endDate = index + 1 < startDates.length ? startDates[index + 1] : data.periods[asset][account].endDate
+                      const transfered = data.earnings[startDate][asset][account].transfered
+                      const balanceFrom = data.earnings[startDate][asset][account].balance + transfered
+                      const balanceTo = data.earnings[endDate][asset][account].balance
+                      const yieldEarned = balanceTo - balanceFrom
+                      const dayCount = (endDate - startDate) / 86_400_000
+
+                      totalDayCount += dayCount
+                      totalSubscription += transfered
+                      totalYieldEarned += yieldEarned
+                      firstBalance ??= balanceFrom
+                      lastBalance = balanceTo
+
+                      return (
+                        <tr key={index}>
+                          <td>{format.asLongDate(startDate)}</td>
+                          <td>{format.asLongDate(endDate)}</td>
+                          <td className="num-cell">{dayCount}</td>
+                          <td className="num-cell">{format.asDecimal(transfered)}</td>
+                          <td className="num-cell">{format.asDecimal(balanceFrom)}</td>
+                          <td className="num-cell">{format.asDecimal(balanceTo)}</td>
+                          <td className="num-cell">{format.asDecimal(yieldEarned)}</td>
+                          <td className="num-cell">{format.asDecimal(yieldEarned / dayCount)}</td>
+                          <td className="num-cell"></td>
+                        </tr>
+                      )
+                    })}
+                  </tbody>
+                  <tfoot>
+                    <tr>
+                      <th></th>
+                      <th></th>
+                      <th className="num-cell">{totalDayCount}</th>
+                      <th className="num-cell">{format.asDecimal(totalSubscription)}</th>
+                      <th className="num-cell">{format.asDecimal(firstBalance)}</th>
+                      <th className="num-cell">{format.asDecimal(lastBalance)}</th>
+                      <th className="num-cell">{format.asDecimal(totalYieldEarned)}</th>
+                      <th className="num-cell">{format.asDecimal(totalYieldEarned / totalDayCount)}</th>
+                      <th className="num-cell"></th>
+                    </tr>
+                  </tfoot>
+                </table>
+              )
+            })}
+          </Fragment>
+        ))}
+      </Fragment>
     )
   }
 
@@ -46,8 +102,7 @@ export default function Yield() {
       <Head>
         <title>Yield Earnings</title>
       </Head>
-      <h1>Yield Earnings</h1>
-      <div className="space-y-12 w-700">
+      <div className="space-y-12">
         {earnings}
       </div>
     </Layout>
