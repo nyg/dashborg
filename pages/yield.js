92932d84b9c27212b6788206119851250d7d4125
nyg <nyg@users.noreply.github.com>
2021-05-24 17:51:21 +0200

    Remove unnecessary React Fragment

--- pages/yield.js
+++ pages/yield.js
@@ -18,68 +18,63 @@ export default function Yield() {
     earnings = <div className="text-center pt-4">Loading data…</div>
   }
   else {
-    earnings = (
-      <Fragment>
-        {Object.keys(data.earnings).map(asset =>
-          <Fragment key={asset}>
-            {Object.keys(data.earnings[asset]).map(account => {
-              const totalRow = data.earnings[asset][account].pop()
-              return (
-                <table key={`${asset}-${account}`}>
-                  <caption>{asset} — {account}</caption>
-                  <thead className="text-right">
-                    <tr>
-                      <th colSpan={4} className="v-sep">Period</th>
-                      <th colSpan={2} className="v-sep">Balance</th>
-                      <th colSpan={3} className="pr-2">Yield</th>
-                    </tr>
-                    <tr>
-                      <th>Start</th>
-                      <th>End</th>
-                      <th>Days</th>
-                      <th className="v-sep">Transfered</th>
-                      <th>Start</th>
-                      <th className="v-sep">End</th>
-                      <th>Earned</th>
-                      <th>Average</th>
-                      <th className="pr-2">APY</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data.earnings[asset][account].map(period => (
-                      <tr key={period.startDate}>
-                        <td className="text-right">{format.asLongDate(period.startDate)}</td>
-                        <td className="text-right">{format.asLongDate(period.endDate)}</td>
-                        <TdNum>{period.dayCount}</TdNum>
-                        <TdNum className="v-sep">{period.transfered}</TdNum>
-                        <TdNum>{period.startBalance}</TdNum>
-                        <TdNum className="v-sep">{period.endBalance}</TdNum>
-                        <TdNum>{period.yieldEarned}</TdNum>
-                        <TdNum>{period.dailyAverage}</TdNum>
-                        <td className="pr-2"></td>
-                      </tr>
-                    ))}
-                  </tbody>
-                  <tfoot>
-                    <tr>
-                      <th></th>
-                      <th></th>
-                      <TdNum>{totalRow.dayCount}</TdNum>
-                      <TdNum className="v-sep">{totalRow.transfered}</TdNum>
-                      <TdNum>{totalRow.startBalance}</TdNum>
-                      <TdNum className="v-sep">{totalRow.endBalance}</TdNum>
-                      <TdNum>{totalRow.yieldEarned}</TdNum>
-                      <TdNum>{totalRow.dailyAverage}</TdNum>
-                      <th className="pr-2"></th>
-                    </tr>
-                  </tfoot>
-                </table>
-              )
-            })}
-          </Fragment>
-        )}
-      </Fragment>
-    )
+    earnings =
+      Object.keys(data.earnings).map(asset =>
+        Object.keys(data.earnings[asset]).map(account => {
+          const totalRow = data.earnings[asset][account].slice(-1)[0]
+          return (
+            <table key={`${asset}-${account}`}>
+              <caption>{asset} — {account}</caption>
+              <thead className="text-right">
+                <tr>
+                  <th colSpan={4} className="v-sep">Period</th>
+                  <th colSpan={2} className="v-sep">Balance</th>
+                  <th colSpan={3} className="pr-2">Yield</th>
+                </tr>
+                <tr>
+                  <th>Start</th>
+                  <th>End</th>
+                  <th>Days</th>
+                  <th className="v-sep">Transfered</th>
+                  <th>Start</th>
+                  <th className="v-sep">End</th>
+                  <th>Earned</th>
+                  <th>Average</th>
+                  <th className="pr-2">APY</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.earnings[asset][account].slice(0, -1).map(period => (
+                  <tr key={period.startDate}>
+                    <td className="text-right">{format.asLongDate(period.startDate)}</td>
+                    <td className="text-right">{format.asLongDate(period.endDate)}</td>
+                    <TdNum>{period.dayCount}</TdNum>
+                    <TdNum className="v-sep">{period.transfered}</TdNum>
+                    <TdNum>{period.startBalance}</TdNum>
+                    <TdNum className="v-sep">{period.endBalance}</TdNum>
+                    <TdNum>{period.yieldEarned}</TdNum>
+                    <TdNum>{period.dailyAverage}</TdNum>
+                    <td className="pr-2"></td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <th></th>
+                  <th></th>
+                  <TdNum>{totalRow.dayCount}</TdNum>
+                  <TdNum className="v-sep">{totalRow.transfered}</TdNum>
+                  <TdNum>{totalRow.startBalance}</TdNum>
+                  <TdNum className="v-sep">{totalRow.endBalance}</TdNum>
+                  <TdNum>{totalRow.yieldEarned}</TdNum>
+                  <TdNum>{totalRow.dailyAverage}</TdNum>
+                  <th className="pr-2"></th>
+                </tr>
+              </tfoot>
+            </table>
+          )
+        })
+      )
   }
 
   return (
