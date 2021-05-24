e3ccb8cdd9cdcae4ee279f5d24f163850bfed18a
nyg <nyg@users.noreply.github.com>
2021-05-24 17:49:17 +0200

    Add footer-image-link component and improve general style

--- components/active-link.js
+++ components/active-link.js
@@ -4,9 +4,9 @@ import { useRouter } from 'next/router'
 export default function ActiveLink({ href, children }) {
 
   const { asPath } = useRouter()
-  const style = asPath === href
-    ? 'bg-gray-400 p-2 rounded-lg text-gray-50 hover:text-gray-50 no-underline w-full'
-    : 'bg-gray-100 p-2 rounded-lg text-gray-500 hover:text-gray-700 no-underline w-full'
+  const style = asPath == href
+    ? 'bg-gray-400 p-1 rounded text-gray-50 hover:text-gray-50 no-underline w-full'
+    : 'bg-gray-100 p-1 rounded text-gray-500 hover:text-gray-700 no-underline w-full'
 
   return (
     <Link href={href} >
--- components/layout.js
+++ components/layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import ActiveLink from './active-link'
+import FooterImageLink from './footer-image-link'
 
 export default function Layout({ children, name }) {
 
@@ -16,15 +16,15 @@ export default function Layout({ children, name }) {
       <main className="flex flex-col h-screen text-gray-600">
 
         <header className="bg-gray-100">
-          <div className="lg:max-w-4xl pt-8 pb-6 pl-16">
-            <h1 className="text-4xl text-gray-900 font-bold">DashBorg</h1>
+          <div className="lg:max-w-5xl pt-6 pb-6 pl-6">
+            <h1 className="text-3xl text-gray-900 font-bold">DashBorg</h1>
             <p className="text-sm">The dashboard for your <a href="http://swissborg.com/">SwissBorg</a> account.</p>
           </div>
         </header>
 
         <nav className="bg-gray-300">
-          <div className="lg:max-w-4xl pt-2 pb-2">
-            <div className="m-auto flex flex-row w-3/5 text-center text-sm space-x-6">
+          <div className="lg:max-w-5xl pt-2 pb-2">
+            <div className="m-auto flex flex-row w-3/5 text-center text-sm space-x-4">
               <ActiveLink href="/">Home</ActiveLink>
               <ActiveLink href="/funding">Funding</ActiveLink>
               <ActiveLink href="/yield">Smart Yield</ActiveLink>
@@ -34,16 +34,14 @@ export default function Layout({ children, name }) {
         </nav>
 
         <div className="text-sm">
-          <div className="lg:max-w-5xl lg:p-4">
+          <div className="lg:max-w-5xl pl-6 pt-8 pb-8">
             {children}
           </div>
         </div>
 
         <footer className="bg-gray-200 mt-auto">
-          <div className="lg:max-w-4xl pt-2 pb-2 text-center">
-            <a href="https://github.com/nyg/dashborg">
-              <Image src="/gh-dark.png" alt="github" width="20" height="20" />
-            </a>
+          <div className="lg:max-w-5xl pt-2 pb-2 text-center leading-3">
+            <FooterImageLink href="https://github.com/nyg/dashborg" src="/github.png" alt="github" />
           </div>
         </footer>
       </main>
