d00644bfe70027e1ed54996ce36f87d7027cec1c
nyg <nyg@users.noreply.github.com>
2021-05-24 17:51:00 +0200

    Add date time format function

--- utils/format.js
+++ utils/format.js
@@ -2,6 +2,7 @@ const locales = typeof navigator !== 'undefined' ? navigator.language : 'en-GB'
 
 const shortDateFormatter = new Intl.DateTimeFormat(locales, { month: 'short', day: 'numeric' })
 const longDateFormatter = new Intl.DateTimeFormat(locales, { year: 'numeric', month: 'short', day: 'numeric' })
+const dateTimeFormatter = new Intl.DateTimeFormat(locales, { dateStyle: 'short', timeStyle: 'short' })
 const percentageFormatter = new Intl.NumberFormat(locales, { style: 'percent', minimumFractionDigits: 2, maximumFractionDigits: 2 })
 
 /* Dates */
@@ -12,6 +13,10 @@ export function asLongDate(timestamp) {
 
 export function asShortDate(timestamp) {
   return shortDateFormatter.format(timestamp)
+}
+
+export function asDateTime(timestamp) {
+  return dateTimeFormatter.format(timestamp)
 }
 
 /* Percentages */
