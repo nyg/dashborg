7cae1c567f65587e6b02caabc0dfb824cd1bdc31
nyg <nyg@users.noreply.github.com>
2022-02-10 22:06:53 +0100

    Fix for dependency update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
-
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: false,
+  content: [
+    './{components,pages}/**/*.js',
+  ],
   theme: {
     extend: {
       width: {
@@ -10,26 +9,5 @@ module.exports = {
       },
     },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    // https://github.com/tailwindlabs/tailwindcss/pull/560#issuecomment-670045304
-    ({ addUtilities, e, theme, variants }) => {
-
-      const colors = flattenColorPalette(theme('borderColor'))
-      delete colors['default']
-
-      const colorMap = Object.keys(colors)
-        .map(color => ({
-          [`.border-t-${color}`]: { borderTopColor: colors[color] },
-          [`.border-r-${color}`]: { borderRightColor: colors[color] },
-          [`.border-b-${color}`]: { borderBottomColor: colors[color] },
-          [`.border-l-${color}`]: { borderLeftColor: colors[color] },
-        }))
-
-      const utilities = Object.assign({}, ...colorMap)
-      addUtilities(utilities, variants('borderColor'))
-    },
-  ],
+  plugins: [],
 }
