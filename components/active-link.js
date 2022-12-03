f258dfb062cde3aaeb85115d8bcfa27774cf842f
nyg <nyg@users.noreply.github.com>
2022-12-03 12:03:17 +0100

    Update Link usage

--- components/active-link.js
+++ components/active-link.js
@@ -9,10 +9,8 @@ export default function ActiveLink({ href, children }) {
     : 'bg-gray-100 p-1 rounded text-gray-500 hover:text-gray-700 no-underline w-full'
 
   return (
-    <Link href={href} >
-      <a className={style}>
-        {children}
-      </a>
+    <Link href={href} className={style}>
+      {children}
     </Link>
   )
 }
